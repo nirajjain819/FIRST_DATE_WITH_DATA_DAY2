699b36edc151b6cc005aa58b6f180c505b7f7ba9
nirajjain819 <102410126+nirajjain819@users.noreply.github.com>
2022-11-11 08:52:34 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class Stack{
+	public:
+		int *arr;
+		int top;
+		int size;
+		Stack(int size){
+			this->size=size;
+			arr=new int[size];
+			top=-1;
+		}
+		
+
+void push(int element){
+			if(size-top>1){
+				top++;
+				arr[top]=element;
+			}
+			else{
+				cout<<"stack overflow ho rha hain"<<endl;
+			}
+		}
+		void pop(){
+			if(top>=0){
+				top--;
+				
+			}
+		else{
+			cout<<"stack underflow"<<endl;
+		}
+	}
+	int peek(){
+		if (top>=0){
+			return arr[top];
+			
+		}
+		else{
+			cout<<"stack is empty"<<endl;
+			return -1;
+		}
+	}
+	bool isEmpty(){
+		if(top==-1){
+			cout<<"stack is empty"<<endl;
+		}
+		else
+		cout<<"stack is not empty"<<endl;
+	
+	}
+};
+	int main(){
+		Stack st(5);
+		st.push(34);
+		st.push(89);
+		cout<<st.peek()<<endl;
+		st.pop();
+		cout<<st.peek()<<endl;
+		st.pop();
+		cout<<st.peek()<<endl;
+		if(st.isEmpty())
+		cout<<"stack is empty"<<endl;
+		return 0;
+	}
+	
